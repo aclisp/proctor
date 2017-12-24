8073a37411c4fed0c8b51aed5fad51d792c8558a
huanghao <huanghao@yy.com>
2017-12-24 14:57:10 +0800

    Fix bug.

--- proctor-common/src/main/java/com/indeed/proctor/common/server/InputContexts.java
+++ proctor-common/src/main/java/com/indeed/proctor/common/server/InputContexts.java
@@ -4,6 +4,8 @@ import com.google.common.collect.Maps;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.mongo.MongoClient;
 import org.apache.log4j.Logger;
@@ -24,7 +26,7 @@ public class InputContexts {
         inputContext.put(isBrandNewUser, false);
 
         // 去海度查询设备最后登录的时间
-        String hiidoServiceURL = "http://14.17.108.47:8518/service/read";
+        String hiidoServiceURL = "http://szhiidocosevice.yy.com/service/read";
         httpClient
                 .postAbs(hiidoServiceURL, response -> {
                     int staticCode = response.statusCode();
@@ -35,9 +37,9 @@ public class InputContexts {
                     }
                     response.bodyHandler(body -> {
                         JsonObject result = new JsonObject(body);
-                        JsonObject data = result.getJsonObject("data", new JsonObject());
-                        if (data.containsKey("dt")) {
-                            LOGGER.debug("device `" + deviceId + "` -> dt `" + data.getString("dt") + "`");
+                        JsonArray data = result.getJsonArray("data", new JsonArray());
+                        if (data.size() > 0 && data.getJsonObject(0).containsKey("dt")) {
+                            LOGGER.debug("device `" + deviceId + "` -> dt `" + data.getJsonObject(0).getString("dt") + "`");
                             resultHandler.handle(inputContext);
                             return;
                         }
@@ -52,6 +54,9 @@ public class InputContexts {
                                     }
                                     resultHandler.handle(inputContext);
                                 });
+                    }).exceptionHandler(t -> {
+                        LOGGER.error("parse response from `" + hiidoServiceURL + "` got " + t.toString());
+                        resultHandler.handle(inputContext);
                     });
                 })
                 .putHeader("content-type", "text/plain")
@@ -74,7 +79,10 @@ public class InputContexts {
 
     public static void main(final String[] args) {
         Vertx vertx = Vertx.vertx();
-        HttpClient httpClient = vertx.createHttpClient();
+        HttpClientOptions httpClientOptions = new HttpClientOptions()
+                .setConnectTimeout(2000)
+                .setIdleTimeout(2000);
+        HttpClient httpClient = vertx.createHttpClient(httpClientOptions);
         JsonObject mongoConfig = new JsonObject().put("connection_string", "mongodb://172.27.142.6:27017/abtest");
         MongoClient mongoClient = MongoClient.createShared(vertx, mongoConfig);
         Handler<Map<String, Object>> h = res -> {
@@ -82,9 +90,11 @@ public class InputContexts {
         };
 
         get(httpClient, mongoClient, "", "", h);
+        get(httpClient, mongoClient, "", "0", h);
         get(httpClient, mongoClient, "", "6c2ca739d48197332e2fef404811775acbc7ead6", h);
         get(httpClient, mongoClient, "", "0768c3e8b7372379c70e26bd7ebb5c75db8303d3", h);
         get(httpClient, mongoClient, "", "881abe546d6743c8a894ede986673620", h);
-        get(httpClient, mongoClient, "", "12345", h);
+        get(httpClient, mongoClient, "", "1234567", h);
+        get(httpClient, mongoClient, "", "123456", h);
     }
 }
